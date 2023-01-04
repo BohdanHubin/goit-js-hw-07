d54c1037ade4cde6b60c3f096569a19124b6440b
BohdanHubin <111269462+BohdanHubin@users.noreply.github.com>
2023-01-04 20:47:30 +0200

    добавляет решение дз

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+
+function createGalleryMarkap(item) {
+   
+    return item.map(({ preview, original, description }) => {
+       return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`
+    }).join('')
+};
+
+const allGalleryMarkap = createGalleryMarkap(galleryItems);
+
+galleryRef.innerHTML =allGalleryMarkap
+
+galleryRef.addEventListener('click', onImageClick)
+
+function onImageClick(event) {
+    event.preventDefault();
+
+    if(event.target.nodeName !== "IMG") {
+    return;
+    }
+
+    const imgSource = event.target.dataset.source;
+
+    const instance = basicLightbox.create(`<img src="${imgSource}" width="800" height="600">`)
+
+    instance.show()
+}
+
+
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryRef = document.querySelector('.gallery');
 
-console.log(galleryItems);
+
+function createGalleryMarkap(item) {
+   
+    return item.map(({ preview, original, description }) => {
+       return `<div class="gallery__item">
+  <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>
+</div>`
+    }).join('')
+};
+
+const allGalleryMarkap = createGalleryMarkap(galleryItems);
+
+galleryRef.innerHTML =allGalleryMarkap
+
+new SimpleLightbox('.gallery a', { captions: true, captionsData: 'alt', captionDelay:250,});
+
+
